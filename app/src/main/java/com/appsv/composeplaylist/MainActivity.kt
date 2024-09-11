f49640f1e4358c398d26192f942829f3b6e8cf14
Viraj76 <virajg.7680@gmail.com>
2024-09-12 03:44:33 +0530

    Ignore - counter example

--- app/src/main/java/com/appsv/composeplaylist/MainActivity.kt
+++ app/src/main/java/com/appsv/composeplaylist/MainActivity.kt
@@ -1,33 +1,44 @@
 package com.appsv.composeplaylist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.appsv.composeplaylist.ui.theme.ComposePlaylistTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             ComposePlaylistTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Counter()
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
             }
         }
     }
@@ -65,4 +76,26 @@ fun GreetingPreview() {
     ComposePlaylistTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun Counter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+    Column {
+        Text(
+            modifier = Modifier.padding(start = 30.dp),
+            text = "$count",
+            color = Color.Blue
+        )
+        Button(onClick = {
+            count++
+            Log.d("COUNTTTT", count.toString())
+        }) {
+            Text(text = "Increment")
+        }
+    }
+
+
 }
