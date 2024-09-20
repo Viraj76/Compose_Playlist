5cee2b7cd50b40f08fb0af6c74cfba3fc95fb12b
Viraj76 <virajg.7680@gmail.com>
2024-09-21 00:57:24 +0530

    part 9 done

--- app/src/main/java/com/appsv/composeplaylist/MainActivity.kt
+++ app/src/main/java/com/appsv/composeplaylist/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.appsv.composeplaylist
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
@@ -7,24 +9,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.ScrollableDefaults
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -42,18 +62,27 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.sharp.Close
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ChipColors
+import androidx.compose.material3.ChipElevation
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -78,7 +107,9 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
@@ -97,6 +128,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.content.edit
 import com.appsv.composeplaylist.ui.theme.ComposePlaylistTheme
 import kotlin.math.truncate
@@ -118,6 +150,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -414,15 +447,14 @@ fun ComposableIcons(modifier: Modifier = Modifier) {
         Icon(
             imageVector = Icons.Sharp.Close,
             contentDescription = "Icon",
-            tint =  Color.Green,
+            tint = Color.Green,
 
-        )
+            )
     }
 
 }
 
 
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ComposableTextField(modifier: Modifier = Modifier) {
 
@@ -436,25 +468,25 @@ fun ComposableTextField(modifier: Modifier = Modifier) {
 
     TextField(
         value = tfText,
-        onValueChange = { text->
+        onValueChange = { text ->
             tfText = text
         },
         modifier = Modifier.padding(30.dp),
         label = {
             Text(text = "Email")
         },
-       leadingIcon = {
-           Icon(
-               imageVector = Icons.Default.Email,
-               contentDescription = "Email Icon"
-           )
-       },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Email,
+                contentDescription = "Email Icon"
+            )
+        },
         trailingIcon = {
             IconButton(onClick = {
                 passwordVisible = !passwordVisible
             }) {
                 Icon(
-                    imageVector =Icons.Default.Done,
+                    imageVector = Icons.Default.Done,
                     tint = Color.Blue,
                     contentDescription = "Email Icon"
                 )
@@ -462,7 +494,7 @@ fun ComposableTextField(modifier: Modifier = Modifier) {
 
         },
 
-        visualTransformation = if(!passwordVisible) PasswordVisualTransformation() else VisualTransformation.None,
+        visualTransformation = if (!passwordVisible) PasswordVisualTransformation() else VisualTransformation.None,
         keyboardOptions = KeyboardOptions(
             KeyboardCapitalization.Words,
             keyboardType = KeyboardType.Text,
@@ -494,5 +526,184 @@ fun ComposableTextField(modifier: Modifier = Modifier) {
 }
 
 
+@Composable
+fun ComposableChips(modifier: Modifier = Modifier) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue),
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        var selectedChip by remember {
+            mutableStateOf("All")
+        }
+        FilterChip(
+            onClick = {
+                selectedChip = "All"
+            },
+            selected = selectedChip == "All",
+            label = { Text(text = "All") },
+            leadingIcon = {
+                if (selectedChip == "All") {
+                    Icon(imageVector = Icons.Default.Done, contentDescription = "Settings")
+                }
+            },
+            border = null,
+            colors = FilterChipDefaults.filterChipColors().copy(
+                containerColor = Color.Cyan.copy(0.5f),
+                labelColor = Color.White,
+                leadingIconColor = Color.White
+            )
+        )
+        SuggestionChip(onClick = { /*TODO*/ }, label = { /*TODO*/ })
+        FilterChip(
+            onClick = {
+                selectedChip = "Low"
+            },
+            selected = selectedChip == "Low",
+            label = { Text(text = "Low") },
+            leadingIcon = {
+                if (selectedChip == "Low") {
+                    Icon(imageVector = Icons.Default.Done, contentDescription = "Settings")
+                }
+            },
+            border = null,
+            colors = FilterChipDefaults.filterChipColors().copy(
+                containerColor = Color.Green.copy(0.5f),
+                labelColor = Color.Green,
+                leadingIconColor = Color.Green,
+                selectedContainerColor = Color.Green.copy(0.5f),
+                selectedLabelColor = Color.Green,
+                selectedLeadingIconColor = Color.Green,
+
+                )
+        )
+        FilterChip(
+            onClick = {
+                selectedChip = "Medium"
+            },
+            selected = selectedChip == "Medium",
+            label = { Text(text = "Medium") },
+            leadingIcon = {
+                if (selectedChip == "Medium") {
+                    Icon(imageVector = Icons.Default.Done, contentDescription = "Settings")
+                }
+            },
+            border = null,
+            colors = FilterChipDefaults.filterChipColors().copy(
+                containerColor = Color.Yellow.copy(0.5f),
+                labelColor = Color.Yellow,
+                leadingIconColor = Color.Yellow
+            )
+        )
+        FilterChip(
+            onClick = { selectedChip = "Hard" },
+            selected = selectedChip == "Hard",
+            label = { Text(text = "Hard") },
+            leadingIcon = {
+                if (selectedChip == "Hard") {
+                    Icon(imageVector = Icons.Default.Done, contentDescription = "Settings")
+                }
+            },
+            border = null,
+            colors = FilterChipDefaults.filterChipColors().copy(
+                containerColor = Color.Red.copy(0.5f),
+                labelColor = Color.Red,
+                leadingIconColor = Color.Red
+            )
+        )
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ComposableLazy(modifier: Modifier = Modifier) {
+    LazyColumn(
+        contentPadding = PaddingValues(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items(cricketPlayersList) { it ->
+            val name = it.name
+            val color = it.color
+            CricketersItem(name = name, color = color)
+        }
+    }
+}
+
+data class Cricketer(
+    val name: String,
+    val color: Color
+)
+
+val cricketPlayersList = listOf(
+
+    Cricketer("Virat", Color.Blue),
+    Cricketer("Rohit", Color.Green),
+    Cricketer("Surya", Color.Black),
+    Cricketer("Rishabh", Color.Magenta),
+    Cricketer("Pandya", Color.Red),
+    Cricketer("Jaddu", Color.DarkGray),
+    Cricketer("Kuldeep", Color.Black),
+    Cricketer("Chahal", Color.Blue),
+    Cricketer("Bumrah", Color.Green),
+    Cricketer("Siraj", Color.Yellow),
+    Cricketer("Shami", Color.Magenta),
+    Cricketer("Shami", Color.Magenta),
+    Cricketer("Shami", Color.Magenta),
+    Cricketer("Shami", Color.Magenta),
+)
+
+
+@Composable
+fun CricketersItem(
+    modifier: Modifier = Modifier,
+    name : String = "Virat",
+    color : Color = Color.Blue
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(
+                elevation = 10.dp,
+                spotColor = color,
+                shape = RoundedCornerShape(15.dp)
+            ),
+        color = Color.White,
+        shadowElevation = 10.dp,
+        shape = RoundedCornerShape(15.dp),
+        border = BorderStroke(1.dp, color)
+    ) {
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Spacer(modifier = Modifier.width(20.dp))
+            Icon(
+                modifier = Modifier
+                    .size(45.dp)
+                    .clip(RoundedCornerShape(25.dp))
+                    .background(color),
+                imageVector = Icons.Default.Person,
+                contentDescription = "Person",
+                tint = Color.White
+            )
+            Spacer(modifier = Modifier.width(15.dp))
+            Text(
+                modifier =  Modifier.padding(10.dp),
+                text = name,
+                style = TextStyle(
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    shadow = Shadow(
+                        color = color,
+                        blurRadius = 20f
+                    )
+                )
+            )
+        }
+
+
+    }
+}
 
 
