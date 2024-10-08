8e60530ec35165b9d59d071696e7882b93bc0c20
Viraj76 <virajg.7680@gmail.com>
2024-10-08 23:10:41 +0530

    ..

--- app/src/main/java/com/appsv/composeplaylist/SideEffects.kt
+++ app/src/main/java/com/appsv/composeplaylist/SideEffects.kt
@@ -1,12 +1,8 @@
 package com.appsv.composeplaylist
 
-import android.content.Context
-import android.graphics.Paint.Align
-import android.hardware.camera2.CameraManager
 import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -18,14 +14,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.RoundRect
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.LifecycleOwner
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 
 @Composable
@@ -101,6 +101,8 @@ fun ComposableSideEffect(modifier: Modifier = Modifier) {
    }
 
 }
+
+
 // network call
 fun getData() : List<String> {
 
