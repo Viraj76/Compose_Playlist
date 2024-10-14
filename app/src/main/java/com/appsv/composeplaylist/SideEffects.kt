ab1788bc35d309dddbea50cbe88f20c2f7b5ed7a
Viraj76 <virajg.7680@gmail.com>
2024-10-14 23:58:58 +0530

    Part 19 Done

--- app/src/main/java/com/appsv/composeplaylist/SideEffects.kt
+++ app/src/main/java/com/appsv/composeplaylist/SideEffects.kt
@@ -5,14 +5,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
@@ -20,10 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kotlinx.coroutines.async
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -34,26 +34,23 @@ fun ComposableSideEffects(modifier: Modifier = Modifier) {
     var count by remember { mutableIntStateOf(0) }
 
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
         Column {
 
-            Button(
-                onClick = {
-                    count++
-                }
-            ) {
+            Button(onClick = {
+                count++
+            }) {
                 Text("Increment $count")
             }
 
-           LaunchedEffect(
-               key1 = count,
-           ) {
-               delay(2000)
-               val data = getData()
-               Log.d("Papaya Coders", "$data $count")
-           }
+            LaunchedEffect(
+                key1 = count,
+            ) {
+                delay(2000)
+                val data = getData()
+                Log.d("Papaya Coders", "$data $count")
+            }
         }
 
     }
@@ -67,15 +64,13 @@ fun ComposableDisposableEffect(modifier: Modifier = Modifier) {
     var clicked by remember { mutableStateOf(false) }
 
     DisposableEffect(key1 = clicked) {
-        Log.d("TAG" , "Resource Occupied")
+        Log.d("TAG", "Resource Occupied")
 
         onDispose {
-            Log.d("TAG" , "Resource Released")
+            Log.d("TAG", "Resource Released")
         }
     }
-    Button(
-        onClick = {clicked = !clicked}
-    ) {
+    Button(onClick = { clicked = !clicked }) {
         Text(("Click"))
     }
 
@@ -87,18 +82,16 @@ fun ComposableSideEffect(modifier: Modifier = Modifier) {
     var count by remember { mutableIntStateOf(0) }
 
     SideEffect {
-        Log.d("TAG" , "Count $count")
+        Log.d("TAG", "Count $count")
     }
 
-   Column {
-       Button(
-           onClick = {count++}
-       ) {
-           Text(("Click $count"))
-       }
+    Column {
+        Button(onClick = { count++ }) {
+            Text(("Click $count"))
+        }
 
-       Text("Count $count")
-   }
+        Text("Count $count")
+    }
 
 }
 
@@ -109,52 +102,122 @@ fun ComposableRememberScope(modifier: Modifier = Modifier) {
     val scope = rememberCoroutineScope()
 
 
-
     var buttonText by remember { mutableStateOf("Hello") }
-    Button(
-        onClick = {
-            scope.launch {
-                delay(2000)
-                buttonText = "World"
-            }
+    Button(onClick = {
+        scope.launch {
+            delay(2000)
+            buttonText = "World"
         }
-    ) {
+    }) {
         Text(buttonText)
     }
-
 }
 
-@Preview(showBackground = true)
 @Composable
 fun ComposableRememberUpdatedState(modifier: Modifier = Modifier) {
     var count by remember { mutableIntStateOf(0) }
 
-    Button(
-        onClick = {
-            count = Random.nextInt(1,100)
-            Log.d("PapayaCoders" , "Random Count $count")
-        }
-    ) {
+    Button(onClick = {
+        count = Random.nextInt(1, 100)
+        Log.d("PapayaCoders", "Random Count $count")
+    }) {
         Text("Click")
     }
 
     ShowUpdatedValue(count)
 }
 
+
+
+@Composable
+fun ComposableProduceState(modifier: Modifier = Modifier) {
+//    var initialState by remember { mutableStateOf("Loading..") }
+//    LaunchedEffect(key1 = Unit) {
+//        val data = getData()
+//        initialState = if (data.isNotEmpty()) "Data Fetched"
+//        else "Data did not fetched"
+//    }
+    var count by remember { mutableIntStateOf(0) }
+    
+    val dataFetched by produceState("", key1 = count) {
+        Log.d("TAG", "Getting Data")
+        val data = getData()
+        value = if(data.isNotEmpty()) "Data Fetched"
+        else "Data did not fetched"
+        Log.d("TAG", "Got Data")
+
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = {
+                count++
+            }
+        ) { }
+        if (dataFetched == "Loading..") {
+            CircularProgressIndicator()
+        } else {
+            Text(dataFetched)
+        }
+    }
+
+
+}
+
+@Preview
+@Composable
+fun ComposableDerivedStateOf(modifier: Modifier = Modifier) {
+
+    var count by remember { mutableIntStateOf(0) }
+
+    val derivedCount by remember {
+        derivedStateOf {
+            count > 3
+        }
+    }
+
+    Column {
+        Button(
+            onClick = {
+                count++
+            }
+        ) {
+            Text("Button1")
+        }
+
+        if(derivedCount){
+            Log.d("TAG", "Showing Button")
+            Button(
+                onClick = {
+
+                }
+            ) {
+                Text("Button2")
+            }
+        }
+        else{
+            Log.d("TAG", "Hiding Button")
+        }
+    }
+
+}
+
+
 @Composable
 fun ShowUpdatedValue(count: Int) {
     val updatedCount = rememberUpdatedState(count)
     LaunchedEffect(key1 = Unit) {
         delay(10000)
-        Log.d("PapayaCoders" , "Updated Count ${updatedCount.value}")
+        Log.d("PapayaCoders", "Updated Count ${updatedCount.value}")
     }
 }
 
-
 // network call
-fun getData() : List<String> {
-
-    return listOf("Papaya" , "Coders")
+suspend fun getData(): List<String> {
+    delay(4000)
+    return listOf("Papaya", "Coders")
 }
 
 
